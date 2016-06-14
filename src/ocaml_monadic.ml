9ac4b19e6d939245727802b5d1cc4df90a96002c
Zachary Palmer <zep_git@bahj.com>
2016-06-14 13:37:23 -0400

    Rewrite to use ppx_metaquot for compatibility and readability.

--- src/ocaml_monadic.ml
+++ src/ocaml_monadic.ml
@@ -8,153 +8,96 @@ open Longident;;
 let mkident name = { txt = Lident name; loc = !default_loc };;
 
 let ghostify loc =
-    { loc with loc_ghost = true }
+  { loc with loc_ghost = true }
 ;;
 
 let no_label = "";;
 
 let ocaml_monadic_mapper argv =
-    (* We override the expr mapper to catch bind and orzero.  *)
-    { default_mapper with
-      expr = fun mapper expr ->
-        match expr with
-        | { pexp_desc =
-            Pexp_extension( { txt = "bind"; loc }, payload )
-          } ->
-          with_default_loc (ghostify loc) @@ fun () ->
-          (* Matches "bind"-annotated expressions. *)
-          begin
-            match payload with
-            | PStr [
-                { pstr_desc =
-                  Pstr_eval(
-                    { pexp_desc = Pexp_let(Nonrecursive, value_bindings, body)
-                    },
-                    []
-                  )
-                }
-              ] ->
-              (* This is a let%bind expression!  It's of the form
-                   let%bind $p1 = $e1 and ... and $pn = $en in $e0
-                 and we want it to take the form
-                   bind $e1 (fun $p1 -> ... bind $en (fun $pn -> ...) ...)
-              *)
-              let rec bind_wrap value_bindings' =
-                match value_bindings' with
-                | { pvb_pat = bind_pattern
-                  ; pvb_expr = bind_expr
-                  ; pvb_attributes = []
-                  ; pvb_loc = bind_loc
-                  }::value_bindings'' ->
-                  (* Recurse and then wrap the resulting body. *)
-                  let body' = bind_wrap value_bindings'' in
-                  (* This is the name of the "bind" function. *)
-                  let bind_ident = Exp.ident @@ mkident "bind" in
-                  (* This is the function we will be calling. *)
-                  let cont_function =
-                    Exp.fun_ no_label None bind_pattern body'
-                  in
-                  (* And finally, here's the wrapped expression. *)
-                  Exp.apply bind_ident
-                    [ (no_label, mapper.expr mapper bind_expr)
-                    ; (no_label, cont_function)
-                    ]
-                | _ ->
-                  (* Nothing left to do.  Just return the body. *)
-                  mapper.expr mapper body
-              in
-              bind_wrap value_bindings
-            | _ -> expr
-          end
-        | { pexp_desc =
-            Pexp_extension( { txt = "orzero"; loc }, payload )
-          } ->
-          with_default_loc (ghostify loc) @@ fun () ->
-          (* Matches "orzero"-annotated expressions. *)
-          begin
-            match payload with
-            | PStr [
-                { pstr_desc =
-                  Pstr_eval(
-                    { pexp_desc =
-                      Pexp_let(Nonrecursive, value_bindings, body)
-                    },
-                    []
-                  )
-                }
-              ] ->
-              (* This is a let%orzero expression.  It's of the form
-                   let%orzero $p1 = $e1 and ... and $pn = $en in $e0
-                 and we want it to take the form
-                   match $e1 with
-                   | $p1 -> (match $e2 with
-                             | $p2 -> ...
-                                      (match $en with
-                                       | $pn -> $e0
-                                       | _ -> zero ())
-                             | _ -> zero ())
-                   | _ -> zero ()
-              *)
-              let rec orzero_wrap value_bindings' =
-                match value_bindings' with
-                | { pvb_pat = orzero_pattern
-                  ; pvb_expr = orzero_expr
-                  ; pvb_attributes = []
-                  ; pvb_loc = orzero_loc
-                  }::value_bindings'' ->
-                  (* This is the name of the "zero" function. *)
-                  let zero_ident = Exp.ident @@ mkident "zero" in
-                  (* This is the recursive call for the success branch. *)
-                  let success_branch = orzero_wrap value_bindings'' in
-                  (* For the failure branch, we call zero. *)
-                  let unit_value =
-                    Exp.construct (mkident "()") None
-                  in
-                  let failure_branch =
-                    Exp.apply zero_ident [(no_label, unit_value)]
-                  in
-                  Exp.match_ (mapper.expr mapper orzero_expr)
-                    [ Exp.case orzero_pattern success_branch
-                    ; Exp.case (Pat.any ()) failure_branch
-                    ]
-                | _ ->
-                  (* Nothing left to do.  Just return the body. *)
-                  mapper.expr mapper body
-              in
-              orzero_wrap value_bindings
-            | _ -> expr
-          end
-        | { pexp_desc =
-            Pexp_sequence(
-              { pexp_desc = Pexp_extension({ txt = "guard" }, payload)
-              },
-              body_expr
-            )
-          } ->
-          begin
-            match payload with
-            | PStr [{ pstr_desc = Pstr_eval(guard_expr, attrs) }] ->
-              (* This is a sequenced expression with a [%guard ...] extension.  It
-                 takes the form
-                   [%guard expr']; expr
-                 and we want it to take the form
-                   if expr' then expr else zero ()
-              *)
-              let unit_value = Exp.construct (mkident "()") None in
-              let zero_ident = Exp.ident @@ mkident "zero" in
-              let guard_expr' =
-                List.fold_left
-                  Exp.attr (mapper.expr mapper guard_expr) attrs
-              in
-              let body_expr' = mapper.expr mapper body_expr in
-              let zero_invocation =
-                Exp.apply zero_ident [(no_label, unit_value)]
-              in
-              Exp.ifthenelse guard_expr' body_expr' (Some zero_invocation)
-            | _ -> expr
-          end
-        | _ -> default_mapper.expr mapper expr
-    }
+  (* We override the expr mapper to catch bind and orzero.  *)
+  { default_mapper with
+    expr = fun mapper expr ->
+      match expr with
+      | [%expr [%bind [%e? expr]]] ->
+        (* Matches "bind"-annotated expressions. *)
+        begin
+          match expr.pexp_desc with
+          | Pexp_let(Nonrecursive, value_bindings, body) ->
+            (* This is a let%bind expression!  It's of the form
+                 let%bind $p1 = $e1 and ... and $pn = $en in $e0
+               and we want it to take the form
+                 bind $e1 (fun $p1 -> ... bind $en (fun $pn -> ...) ...)
+            *)
+            let rec bind_wrap value_bindings' =
+              match value_bindings' with
+              | { pvb_pat = bind_pattern
+                ; pvb_expr = bind_expr
+                ; pvb_attributes = []
+                ; pvb_loc = bind_loc
+                }::value_bindings'' ->
+                (* Recurse and then wrap the resulting body. *)
+                let body' = bind_wrap value_bindings'' in
+                let cont_function =
+                  Exp.fun_ no_label None bind_pattern body'
+                in
+                [%expr
+                  bind [%e mapper.expr mapper bind_expr] [%e cont_function]]
+              | _ ->
+                (* Nothing left to do.  Just return the body. *)
+                mapper.expr mapper body
+            in
+            bind_wrap value_bindings
+          | _ -> expr
+        end
+      | [%expr [%orzero [%e? expr]]] ->
+        (* Matches "orzero"-annotated expressions. *)
+        begin
+          match expr.pexp_desc with
+          | Pexp_let(Nonrecursive, value_bindings, body) ->
+            (* This is a let%orzero expression.  It's of the form
+                 let%orzero $p1 = $e1 and ... and $pn = $en in $e0
+               and we want it to take the form
+                 match $e1 with
+                 | $p1 -> (match $e2 with
+                           | $p2 -> ...
+                                    (match $en with
+                                     | $pn -> $e0
+                                     | _ -> zero ())
+                           | _ -> zero ())
+                 | _ -> zero ()
+            *)
+            let rec orzero_wrap value_bindings' =
+              match value_bindings' with
+              | { pvb_pat = orzero_pattern
+                ; pvb_expr = orzero_expr
+                ; pvb_attributes = []
+                ; pvb_loc = orzero_loc
+                }::value_bindings'' ->
+                (* Recurse and then wrap the resulting body. *)
+                let body' = orzero_wrap value_bindings'' in
+                [%expr
+                  match [%e mapper.expr mapper orzero_expr] with
+                  | [%p orzero_pattern] -> [%e body']
+                  | _ -> zero ()
+                ]
+              | _ ->
+                (* Nothing left to do.  Just return the body. *)
+                mapper.expr mapper body
+            in
+            orzero_wrap value_bindings
+          | _ -> expr
+        end
+      | [%expr [%guard [%e? guard_expr]]; [%e? body_expr]] ->
+        (* This is a sequenced expression with a [%guard ...] extension.  It
+           takes the form
+             [%guard expr']; expr
+           and we want it to take the form
+             if expr' then expr else zero ()
+        *)
+        mapper.expr mapper
+          [%expr if [%e guard_expr] then [%e body_expr] else zero ()]
+      | _ -> default_mapper.expr mapper expr
+  }
 ;;
 
 let () = register "bind" ocaml_monadic_mapper;;
--- src_test/test.ml
+++ src_test/test.ml
@@ -80,6 +80,15 @@ let chained_guard_test =
   return y
 ;;
 
+let zero_guard_interleave_test =
+  let open NondeterminismMonad in
+  let xss = [[0;1;2];[3;4];[5;6;7];[];[8;9;10]] in
+  let%bind xs = xss in
+  let%orzero [a;b;c] = xs in
+  [%guard a <> 0];
+  return c
+;;
+
 let halt_with s = print_endline s; exit 1;;
 
 let () =
@@ -89,63 +98,71 @@ let () =
     | _ ->
       halt_with
         ("Unexpected value from nondeterminism test: " ^
-          string_of_list string_of_int nondeterminism_test)
+         string_of_list string_of_int nondeterminism_test)
   end;
   begin
     match nested_bind_test with
     | [3;4;5] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from nested bind test: " ^
-          string_of_list string_of_int nested_bind_test)
+        ("Unexpected value from nested bind test: " ^
+         string_of_list string_of_int nested_bind_test)
   end;
   begin
     match orzero_test_first with
     | [3] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from first orzero test: " ^
-          string_of_list string_of_int orzero_test_first)
+        ("Unexpected value from first orzero test: " ^
+         string_of_list string_of_int orzero_test_first)
   end;
   begin
     match orzero_test_second with
     | [] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from second orzero test: " ^
-          string_of_list string_of_int orzero_test_second)
+        ("Unexpected value from second orzero test: " ^
+         string_of_list string_of_int orzero_test_second)
   end;
   begin
     match orzero_test_third with
     | [] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from third orzero test: " ^
-          string_of_list string_of_int orzero_test_third)
+        ("Unexpected value from third orzero test: " ^
+         string_of_list string_of_int orzero_test_third)
   end;
   begin
     match orzero_test_fourth with
     | [4] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from fourth orzero test: " ^
-          string_of_list string_of_int orzero_test_fourth)
+        ("Unexpected value from fourth orzero test: " ^
+         string_of_list string_of_int orzero_test_fourth)
   end;
   begin
     match guard_test with
     | [2;4] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from guard test: " ^
-        string_of_list string_of_int guard_test)
+        ("Unexpected value from guard test: " ^
+         string_of_list string_of_int guard_test)
   end;
   begin
     match chained_guard_test with
     | [2;6;8] -> ()
     | _ ->
       halt_with
-      ("Unexpected value from chained guard test: " ^
-        string_of_list string_of_int chained_guard_test)
+        ("Unexpected value from chained guard test: " ^
+         string_of_list string_of_int chained_guard_test)
+  end;
+  begin
+    match zero_guard_interleave_test with
+    | [7;10] -> ()
+    | _ ->
+      halt_with
+        ("Unexpected value from zero/guard interleave test: " ^
+         string_of_list string_of_int zero_guard_interleave_test)
   end;
   print_endline "All tests passed."
 ;;
